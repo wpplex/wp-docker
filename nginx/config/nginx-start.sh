35a31ef6f751f9cc154a404eee05349823485769
Abid Omar <contact@omarabid.com>
2017-08-01 23:19:09 +0100

    - Updated Nginx to 1.13.3
    - Moved SSL certificate creation to Docker container.

--- nginx/config/nginx-start.sh
+++ nginx/config/nginx-start.sh
@@ -2,4 +2,9 @@
 sed -i "s/%root-path%/$PROJECT_NAME/" /etc/nginx/conf.d/default.conf
 sed -i "s/%ip-address%/$PHP_PORT_9000_TCP_ADDR/" /etc/nginx/conf.d/default.conf
 
-nginxz
+# SSL Certificate
+mkdir /etc/nginx/ssl
+cd /etc/nginx/ssl
+openssl req -newkey rsa:2048 -nodes -subj "/C=US/ST=New York/L=Mn/O=Monsanto/CN=monsanto.com" -keyout nginx.key -x509 -days 365 -out nginx.crt
+
+nginx
